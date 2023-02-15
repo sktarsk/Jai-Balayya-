91e360cd2594b0b31c7a7cd5867d80ee93aa9425
sktarsk <117266861+sktarsk@users.noreply.github.com>
2023-02-15 15:58:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,23 +382,21 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-
-    url = f'https://api.shareus.in/shortLink'
-    params = {'token': URL_SHORTNER_WEBSITE_API,
-              'link': link,
-              'format': 'json'
+    url = f'https://shorturllink.in/api'
+    params = {'api': SHORTNER_API,
+              'url': link,
               }
 
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json(content_type='text/html')
+                data = await response.json()
                 if data["status"] == "success":
-                    return data['shortlink']
+                    return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://api.shareus.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://{SHORTNER_SITE}/api?api={SHORTNER_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://api.shareus.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'{SHORTNER_SITE}/api?api={SHORTNER_API}&link={link}'
